c85dd9fbe0931be808d28e74fb6ee6e6501d0b00
junaid bakhtiar <junaidbakhtiar@gmail.com>
2024-02-02 03:11:08 -0600

    updated file and add new file for testing

---
+++ src/main/java/test/qa/pages/SerachTest.java
@@ -0,0 +1,10 @@
+package test.qa.pages;
+
+public class SerachTest {
+	
+	public void searchtesting() {
+		
+		System.out.println("TESTING UPDATEd");
+	}
+
+}
--- src/main/java/test/qa/pages/TestPage.java
+++ src/main/java/test/qa/pages/TestPage.java
@@ -1,5 +1,14 @@
 package test.qa.pages;
 
 public class TestPage {
+	public void test1() {
+		
+		System.out.println("Testing GIT 1");
+	}
+	
+	public void testing1() {
+		
+		System.out.println("Testing GIT 22");
+	}
 
 }
